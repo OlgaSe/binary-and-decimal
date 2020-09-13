b0c95de1e15b8bd4416579c93a589d957513c7b3
OlgaS <olga.tka4eva@gmail.com>
2020-09-12 22:56:36 -0700

    Update binary_to_decimal.rb

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,17 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal = 0
+  reverse_binary_array = binary_array.reverse
+  reverse_binary_array.each_with_index do |num, index|
+    decimal += num * 2 ** index
+  end
+  return decimal
 end
+
+binary_array = []
+until binary_array.length == 8 do
+  binary_array << rand(0..1)
+end
+p binary_array
+puts "#{binary_to_decimal(binary_array)}"
